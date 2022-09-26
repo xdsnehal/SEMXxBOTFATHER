734d83343772a1b0da33e4ba7bd31fe4c0b2d5a9
xdsnehal <114375185+xdsnehal@users.noreply.github.com>
2022-09-26 18:18:01 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -21,30 +21,4 @@ print("Enjoy..!  Do Visit @HACKERxSPAM")
 
 if __name__ == "__main__":
     MK.run_until_disconnected()
-    
-if __name__ == "__main__":
-    MK2.run_until_disconnected()
-
-if __name__ == "__main__":
-    MK3.run_until_disconnected()
-    
-if __name__ == "__main__":
-    MK4.run_until_disconnected()
-
-if __name__ == "__main__":
-    MK5.run_until_disconnected()
-    
-if __name__ == "__main__":
-    MK6.run_until_disconnected()
-    
-if __name__ == "__main__":
-    MK7.run_until_disconnected()
-
-if __name__ == "__main__":
-    MK8.run_until_disconnected()
-    
-if __name__ == "__main__":
-    MK9.run_until_disconnected()
-
-if __name__ == "__main__":
-    MK10.run_until_disconnected()    
+   
